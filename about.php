25a808a22c52789a537a6e9ea26e03b57b44f09e
alphaman99 <118074244+alphaman99@users.noreply.github.com>
2022-11-22 17:08:59 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<div style="text-align:center">
+<body bgcolor="lightblue">
+ <center> <h2><b>ABOUT MAJOJOS</b></h2><br></center>
+ <left> <p style="color:#FF0000";><b>OUR CONTACT DETAILS</b></p></left><br>
+<p>EMAIL: zinhleyul@gmail.com</p>
+<p>PHONE NUMBER: 0799447160</p>
+<p>ALTERNATE NUMBER:0795179611 </p><br>
+
+
+
+
+
+
+
+ <left> <p style="color:#FF0000";><b>LOCATION DETAILS</b></p></left><br>
+<p>PRECISE LOCATION:116, R538, White River, 1240</p>
+
+
+
+
+</div>
+
+
+
+
+
+
+</body>
+</html>
